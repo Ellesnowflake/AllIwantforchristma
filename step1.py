8e87a49e0d7825269022bfbf016f1e1dfdc05fc0
Ellesnowflake <64865551+Ellesnowflake@users.noreply.github.com>
2025-11-18 10:15:09 +0200

    Challenge part 1

--- step1.py
+++ step1.py
@@ -1,11 +1,10 @@
 # Fix this so it prints the correct Part 1 of a secret code.
-# Hint: Read the comment carefully.
+# Hint: One character of 'encoded' is junk and should be removed.
 
-encoded = "xaHR0cHM6Ly9naXRodWIuY29tL0VsbGVzbm93Zmxha2UvQWxs"
+encoded = "xaHR0cHM6Ly9naXRodWIuY29tL0VsbGVzbm93Zmxha2UvQWxsSXdhbn"
 
-# The first character of 'encoded' is junk and should be removed.
-# Your goal is to make 'part1' contain the secret string WITHOUT that junk character.
+# Your goal: make 'part1' contain the string without the junk character.
 
-part1 = encoded[0:]  
+part1 = encoded  
 
 print("Part 1:", part1)
