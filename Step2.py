48bbc780108f44fad8c9b52e42cc73da9397027e
Ellesnowflake <64865551+Ellesnowflake@users.noreply.github.com>
2025-11-18 09:41:26 +0200

    Challenge part 2

---
+++ Step2.py
@@ -0,0 +1,7 @@
+encoded = "xSXdhbnRmb3JjaHJpc3RtYS9ibG9iL21haW4vbmFuaS5qcGVn"
+
+# Again, the first character is junk. Your job is to produce the clean Part 2 string.
+
+part2 = encoded  
+
+print("Part 2:", part2)
