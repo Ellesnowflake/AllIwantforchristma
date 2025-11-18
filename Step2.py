d9c171adc4fdce906026f98b32ec253ff2d3011d
Ellesnowflake <64865551+Ellesnowflake@users.noreply.github.com>
2025-11-18 10:22:03 +0200

    part 2

--- Step2.py
+++ Step2.py
@@ -1,6 +1,6 @@
-encoded = "xSXdhbnRmb3JjaHJpc3RtYS9ibG9iL21haW4vbmFuaS5qcGVn"
+encoded = "SXdhbnRmb3JjaHJpc3RtYS9ibG9iL21haW4vbmFuaxS5qcGVn"
 
-# Again, the first character is junk. Your job is to produce the clean Part 2 string.
+# Again, one character is junk. Your job is to produce the clean Part 2 string.
 
 part2 = encoded  
 
