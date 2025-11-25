4b26341720fa680340653951f5256797c017ca8d
Ellesnowflake <64865551+Ellesnowflake@users.noreply.github.com>
2025-11-25 17:58:56 +0200

    Challenge part1

--- step1.py
+++ step1.py
@@ -1,7 +1,7 @@
 # Fix this so it prints the correct Part 1 of a secret code.
 # Hint: One character of 'encoded' is junk and should be removed.
 
-encoded = "xaHR0cHM6Ly9naXRodWIuY29tL0VsbGVzbm93Zmxha2UvQWxsSXdhbn"
+encoded = "xaHR0cHM6Ly9naXRodWIuY29tL0VsbGVzbm93Zmxha2UvQWxsSXd"
 
 # Your goal: make 'part1' contain the string without the junk character.
 
